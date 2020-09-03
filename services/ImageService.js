6b625760fb4381b1a4786687047715b1e3b57ece
bloombar <amos@cs.nyu.edu>
2020-09-03 15:59:13 -0400

    attempting image autorotation

--- services/ImageService.js
+++ services/ImageService.js
@@ -1,8 +1,10 @@
 const sharp = require('sharp')
+const jo = require('jpeg-autorotate')
 const uuidv4 = require('uuid/v4')
 const util = require('util')
 const path = require('path')
 const fs = require('fs')
+const { resolve } = require('path')
 
 // create a promisified version of fs.unlink
 const fsunlink = util.promisify(fs.unlink)
@@ -13,6 +15,23 @@ function ImageService({ config }) {
     const filename = this.filename()
     const filepath = this.filepath(filename)
 
+    // autorotate
+    await jo
+      .rotate(buffer, { quality: 100 })
+      .then(({ buffer, orientation, dimensions, quality }) => {
+        // console.log(`Orientation was ${orientation}`)
+        // console.log(
+        //   `Dimensions after rotation: ${dimensions.width}x${dimensions.height}`
+        // )
+        // console.log(`Quality: ${quality}`)
+        // ...Do whatever you need with the resulting buffer...
+      })
+      .catch((error) => {
+        // console.log(
+        //   'An error occurred when rotating the file: ' + error.message
+        // )
+      })
+
     // resize and store image
     await sharp(buffer)
       .resize(config.maxImageWidth, config.maxImageHeight, {
@@ -21,6 +40,7 @@ function ImageService({ config }) {
       })
       .toFormat('png')
       .toFile(filepath)
+
     // return filename
     return filename
   } // store
