c39ec456974c43f4e80e619e74f2b76dfa866510
bloombar <amos@cs.nyu.edu>
2020-09-27 22:00:16 -0400

    fixing email signature bug

--- services/EmailService.js
+++ services/EmailService.js
@@ -30,15 +30,15 @@ function EmailService({ config }) {
 
   // send the mail!
   this.send = (to, subject, text, unsubscribeLink = true) => {
-    const transporter = this.getTransporter()
-    const mailOptions = this.getMailOptions(to, subject, text)
-
     // add signature
-    text += '\n\n- The Wikistreets.io Team'
+    text += '\n\nSincerely,\nThe Wikistreets.io Team'
     // tack on unsubscribe info, if desired
     if (unsubscribeLink) {
       text += `\n\n--\nWe aim to keep email notifications to a minimum, only sending email when we think it is of direct interest to you.  If you wish to unsubscribe from all email notifications, first log in and then visit https://wikistreets.io/users/unsubscribe/email.`
     }
+
+    const transporter = this.getTransporter()
+    const mailOptions = this.getMailOptions(to, subject, text)
 
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
