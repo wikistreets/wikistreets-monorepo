a34f0f00de9376ce824f56ca31b135b79ff28716
bloombar <amos@cs.nyu.edu>
2021-02-23 21:31:10 -0500

    increasing image sizes

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config() // to get environmental variables
+require("dotenv").config() // to get environmental variables
 //const path = require('path');
 
 const config = {
@@ -8,28 +8,28 @@ const config = {
   },
   mongo: {
     connectionString:
-      process.env.NODE_ENV == 'production'
+      process.env.NODE_ENV == "production"
         ? process.env.MONGODB_CXN_STRING_PROD
         : process.env.MONGODB_CXN_STRING_DEV,
     user:
-      process.env.NODE_ENV == 'production'
+      process.env.NODE_ENV == "production"
         ? process.env.MONGODB_USER_PROD
         : process.env.MONGODB_USER_DEV,
     password:
-      process.env.NODE_ENV == 'production'
+      process.env.NODE_ENV == "production"
         ? process.env.MONGODB_PASS_PROD
         : process.env.MONGODB_PASS_DEV,
   },
   markers: {
-    uploadDirectory: 'public/uploads',
+    uploadDirectory: "public/uploads",
     maxFiles: 10,
-    maxImageWidth: 1200,
-    maxImageHeight: 1200,
+    maxImageWidth: 3500,
+    maxImageHeight: 3500,
     maxImageFileSize: 50 * 1024 * 1024, // 50MB
   },
   map: {
     boundingBoxBuffer: 1, // 1 km
-    uploadDirectory: 'public/uploads',
+    uploadDirectory: "public/uploads",
     maxFiles: 10, // underlying images
     maxImageWidth: 4000,
     maxImageHeight: 4000,
